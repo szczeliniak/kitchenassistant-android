92e018e5ac93468e362df0681afb20e4f8ef7562
Marcin Szczeliniak <marcin@szczeliniak.pl>
2022-04-12 19:17:46 +0200

    login form validation

---
+++ kitchenassistant-android-app/src/main/java/pl/szczeliniak/kitchenassistant/android/ui/utils/ButtonUtils.kt
@@ -0,0 +1,21 @@
+package pl.szczeliniak.kitchenassistant.android.ui.utils
+
+import android.widget.Button
+
+class ButtonUtils {
+
+    companion object {
+
+        fun Button.enable(enable: Boolean) {
+            if (enable) {
+                isEnabled = true
+                alpha = 1F
+            } else {
+                isEnabled = false
+                alpha = 0.6F
+            }
+        }
+
+    }
+
+}
